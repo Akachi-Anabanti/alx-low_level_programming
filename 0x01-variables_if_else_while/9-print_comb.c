7c7e1eb6824369628aa22362312ac509229d2557
Akachi <akachi.anabanti@gmail.com>
2023-03-18 20:11:23 +0000

    corrected code

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,15 @@ int main(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-		if ( i < 57)
+		putchar(i);
+
+		if (i < 57)
 		{
-			putchar(i);
+
 			putchar(44);
 			putchar(32);
-		} else {
-			putchar(i);
 		}
+
 	}
 
 	putchar('\n');

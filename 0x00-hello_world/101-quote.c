2264831689dbdf09d2072d675ed7a4d9facba404
Akachi <akachi.anabanti@gmail.com>
2023-03-18 09:43:52 +0000

    formatted string output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 1 on successful execution
+ */
+int main(void)
+{
+	char name[] = "Dora Korpar";
+	int y = 2015;
+	int m = 10;
+	int d = 19;
+
+	printf("and that piece of art is useful\" - %s, %i-%i-%i\n", name, y, m, d);
+	return (1);
+}

f5edc26448de3af0d0b52107fbf33fef4824a9f6
Akachi <akachi.anabanti@gmail.com>
2023-03-18 02:52:50 +0000

    print without printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 on sucessful execution
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}

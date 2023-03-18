7462306f27546bad17382deed106f7889a5c564d
Akachi <akachi.anabanti@gmail.com>
2023-03-18 19:18:10 +0000

    print digits using ASCII code and putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57 ; ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}

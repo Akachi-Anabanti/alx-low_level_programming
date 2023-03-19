2e6f8e33a1a3dcbd97d2d0f8a1ddd49020a25037
Akachi <akachi.anabanti@gmail.com>
2023-03-19 10:21:25 +0000

    combination of numbers with putchar

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return; 0 on successful run
+ */
+int main(void)
+{
+	int i, j, k,m;
+
+	for (i=48; i <= 57; ++i)
+	{
+		for (j = 48; j <= 57; ++j)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (m = k+1; m <= 57; m++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(m);
+
+					if (i + j + k + m < 227)
+					{
+						putchar(44);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}

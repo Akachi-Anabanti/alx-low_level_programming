f4e036bf0a13fc08449081391d4597b9293ce445
Akachi <akachi.anabanti@gmail.com>
2023-03-22 15:14:00 +0000

    check if an input is a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabets lower or uppercase
+ * @c: The input to check
+ *
+ * Return: 1 if c is alphabet, otherwise 0
+ */
+int _isalpha(unsigned char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+
+	return (0);
+}

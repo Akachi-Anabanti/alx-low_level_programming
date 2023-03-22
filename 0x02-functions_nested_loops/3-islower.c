0505e9488e4450ebd5488d23744e2e3219e03b59
Akachi <akachi.anabanti@gmail.com>
2023-03-22 14:54:54 +0000

    check lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ * @c: the character to be checked
+ *
+ * Return: 1 is c is lower otherwise 0
+ */
+int _islower(unsigned char c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int _putchar(char c); /* prototype of putchar */
 void print_alphabet(void); /* prototype of print_alphabet */
 void print_alphabet_x10(void); /*prototype of print_alphabet */
+int _islower(unsigned char c); /*prototype of _islower */
 
 #endif

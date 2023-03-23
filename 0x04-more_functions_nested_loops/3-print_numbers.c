baa7fe692fd7e7af2ac8b99285062154b91745c6
Akachi <akachi.anabanti@gmail.com>
2023-03-23 23:23:27 +0000

    print numbers only

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0-9
+ */
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 48; j <= 57; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}

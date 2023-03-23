5c52a505cc46ec4d9c7aecee8a09da29ed37f7e3
Akachi <akachi.anabanti@gmail.com>
2023-03-23 23:53:38 +0000

    repeatedly print numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - repeatedly print a range of numbers 0-14
+ */
+void more_numbers(void)
+{
+	int i, j;
+	j = 0;
+
+	do{
+		for(i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+
+		_putchar('\n');
+
+		j++;
+
+	} while(j < 10);
+}

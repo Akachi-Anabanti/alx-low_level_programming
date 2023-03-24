c6dd1e866f4465e679eab51b02c88fe02f873149
Akachi <akachi.anabanti@gmail.com>
2023-03-24 01:23:09 +0000

    betty tested and updated syntax

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
+
+/**
+ * main - entry point function that prints fizz buzz
+ * Return: 0 on successful execution
+ */
+
 int main(void)
 {
 	int i;
-	
+
 	for (i = 0; i <= 100; i++)
 	{
 		if (i == 0)
 			continue;
-
 		else if (i % 3 == 0 && !(i % 5 == 0))
 		{
 			printf("Fizz");
 			printf(" ");
 		}
-		else if (i % 5 == 0 && !(i % 3 ==0))
+		else if (i % 5 == 0 && !(i % 3 == 0))
 		{
 			printf("Buzz");
 			printf(" ");
@@ -31,4 +36,5 @@ int main(void)
 		}
 	}
 	printf("\n");
+	return (0);
 }

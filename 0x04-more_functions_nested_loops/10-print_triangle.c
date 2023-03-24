44ce627c4a08c2cfceb980ef1524afd0ebc21da5
Akachi <akachi.anabanti@gmail.com>
2023-03-24 02:29:29 +0000

    corrected for betty

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,14 +8,15 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if ( size <= 0)
+	if (size <= 0)
+	{
 		_putchar('\n');
-
-	else 
+	}
+	else
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for(j = i; j < size; j++)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}

42f03008ffb175db609793c80f282923828d486b
Akachi <akachi.anabanti@gmail.com>
2023-03-24 01:18:22 +0000

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i <= 100; i++)
+	{
+		if (i == 0)
+			continue;
+
+		else if (i % 3 == 0 && !(i % 5 == 0))
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (i % 5 == 0 && !(i % 3 ==0))
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	printf("\n");
+}

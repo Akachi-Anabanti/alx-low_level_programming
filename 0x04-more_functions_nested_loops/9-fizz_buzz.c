4ed5f4253571fcac56692bfdb1adfec70c29d7b1
Akachi <akachi.anabanti@gmail.com>
2023-03-24 01:36:08 +0000

    corrected code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,23 +17,21 @@ int main(void)
 		else if (i % 3 == 0 && !(i % 5 == 0))
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else if (i % 5 == 0 && !(i % 3 == 0))
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else if (i % 5 == 0 && i % 3 == 0)
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else
 		{
 			printf("%d", i);
-			printf(" ");
 		}
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);

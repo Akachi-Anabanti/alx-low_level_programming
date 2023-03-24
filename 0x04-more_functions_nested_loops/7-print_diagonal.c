de3c4926a72644a350d8e02a5776fccfecfd6f40
Akachi <akachi.anabanti@gmail.com>
2023-03-24 00:13:22 +0000

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints \ for n times
+ * @n: the number of times to print _
+ */
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('\\');
+			i++;
+		}
+	}
+	_putchar('\n');
+}

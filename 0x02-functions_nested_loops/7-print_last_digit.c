b5d9a2645facde893badb9484039aff70ad47ec3
Akachi <akachi.anabanti@gmail.com>
2023-03-26 23:47:56 +0000

    corrected for betty coding style

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,17 +9,13 @@
 
 int print_last_digit(int n)
 {
-        int last;
+	int last;
 
-        if (n < 0)
-                n = -n;
-        last = n % 10;
-                
-        if (last < 0)
-                last = -last;
-        
-        _putchar(last + '0');
-        
-        return (last);
-                
+	if (n < 0)
+		n = -n;
+	last = n % 10;
+	if (last < 0)
+		last = -last;
+	_putchar(last + '0');
+	return (last);
 }

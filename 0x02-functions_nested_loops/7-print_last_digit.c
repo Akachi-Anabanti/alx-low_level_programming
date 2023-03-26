f8b392afd5fca738e946ab2f5e5604c86cf2b5f4
Akachi <akachi.anabanti@gmail.com>
2023-03-26 23:42:06 +0000

    printing last digit of a number with putchar

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit
+ * @n: The number to extract the last digit from
+ *
+ * Return: The last digit
+ */
+
+int print_last_digit(int n)
+{
+        int last;
+
+        if (n < 0)
+                n = -n;
+        last = n % 10;
+                
+        if (last < 0)
+                last = -last;
+        
+        _putchar(last + '0');
+        
+        return (last);
+                
+}

63a15ce3e74fa2a11fc255077c099026d890af69
Akachi <akachi.anabanti@gmail.com>
2023-03-26 23:29:09 +0000

    reverted changes

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,21 +7,27 @@
  */
 int main(void)
 {
-	int i, j;
+	int i, j, k;
 
-	for (i = 0; i < 99; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		for (j = i + 1; j < 100; j++)
+		for (j = i + 1; j <= 57; j++)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
-			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-			if (i != 98)
+			for (k = j + 1; k <= 57; k++)
 			{
-				putchar(',');
-				putchar(' ');
+
+				if (i != j && j != k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (i + j + k < 168)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
 			}
 		}
 	}

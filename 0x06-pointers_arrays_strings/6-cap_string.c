6c657560846fb8b6877e7d5a39ed2ab27af96ef5
Akachi <akachi.anabanti@gmail.com>
2023-03-31 23:50:42 +0000

    changed algorithm

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,53 +8,31 @@
 char *cap_string(char *s)
 {
 	int i = 0;
+	int j;
+	char delim[13];
+
+	delim[0] = ' ';
+	delim[1] = '\t';
+	delim[2] = '\n';
+	delim[3] = ',';
+	delim[4] = ';';
+	delim[5] = '.';
+	delim[6] = '!';
+	delim[7] = '?';
+	delim[8] = '"';
+	delim[9] = '(';
+	delim[10] = ')';
+	delim[11] = '{';
+	delim[12] = '}';
 
 	while (s[i] != '\0')
 	{
+
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
-			switch (s[i - 1])
-			{
-				case ' ':
+			for (j = 0; j <= 12; j++)
+				if (s[i - 1] == delim[j])
 					s[i] = s[i] - 32;
-					break;
-				case '\t':
-					s[i] = s[i] - 32;
-					break;
-				case '\n':
-					s[i] = s[i] - 32;
-					break;
-				case ',':
-					s[i] = s[i] - 32;
-					break;
-				case ';':
-					s[i] = s[i] - 32;
-					break;
-				case '.':
-					s[i] = s[i] - 32;
-					break;
-				case '!':
-					s[i] = s[i] - 32;
-					break;
-				case '?':
-					s[i] = s[i] - 32;
-					break;
-				case '"':
-					s[i] = s[i] - 32;
-					break;
-				case '(':
-					s[i] = s[i] - 32;
-					break;
-				case ')':
-					s[i] = s[i] - 32;
-					break;
-				case '{':
-					s[i] = s[i] - 32;
-					break;
-				case '}':
-					s[i] = s[i] - 32;
-					break;
-			}
 		}
 		i++;
 	}

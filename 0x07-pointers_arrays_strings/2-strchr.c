41c277574a10ca082e97d0db322954a644fc1457
Akachi <akachi.anabanti@gmail.com>
2023-04-03 22:27:39 +0000

    fixed betty error

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,17 +1,17 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * *_strchr - finds the first occurence of c in s
  * @s: string to search
  * @c: character to find
+ * Return: pointer to c
  */
 char *_strchr(char *s, char c)
 {
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
 		s++;
 	}
-	return (NULL);
+	return ('\0');
 }

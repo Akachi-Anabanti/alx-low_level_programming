18998f6783d33c41d6faade31d0646b29b6ebcf6
Akachi <akachi.anabanti@gmail.com>
2023-04-03 19:52:24 +0000

    fill n bytes of memory

---
+++ 0x07-pointers_array_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memet - fills the first n bytes of the memory area
+ * pointed to by s with the constant byte b
+ * @s: the string the be filled
+ * @b: the constant byte
+ * @n: the number of bytes to fill
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *pter;
+	
+	pter = s;
+
+	for (; n - 1 != 0; n--)
+	{
+		s[n] = b;
+	}
+
+	return (pter);
+}

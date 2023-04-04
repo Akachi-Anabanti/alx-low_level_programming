27c12d5f47a139a3aa229b3415f4973d30a0c133
Akachi <akachi.anabanti@gmail.com>
2023-04-04 04:28:28 +0000

    refactored

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,18 +10,15 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
-	char *ptr;
 
-	for (i = 0; needle[i]; i++)
+	for (i = 0; haystack[i]; i++)
 	{
-		for (j = 0; haystack[j]; j++)
+		for (j = 0; needle[j] && haystack[i + j] == needle[j] j++)
 		{
-			if (needle[i] == haystack[j])
-			{
-				ptr = &haystack[j];
-				return (ptr);
-			}
+			break;
 		}
+		if (needle[j] == '\0')
+			return (&haystack[i]);
 	}
-	return ('\0');
+	return (NULL);
 }

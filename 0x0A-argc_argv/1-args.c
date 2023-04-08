598b410ba7506f64ea30d36f20179d6b771e1cd8
Akachi <akachi.anabanti@gmail.com>
2023-04-08 15:54:52 +0000

    prints the number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed to it
+ * @argc - Argument count
+ * @argv - argument vector
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

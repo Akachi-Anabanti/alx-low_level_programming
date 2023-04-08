895602ae7f0e724158c70b18b10664b7da7bf5bf
Akachi <akachi.anabanti@gmail.com>
2023-04-08 15:00:58 +0000

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ * @argc: argument count
+ * @argv: argument vector (a pointer to array of pointers)
+ * Return: 0 on succesful exit
+ */
+int main(int argc, char *argv[])
+{
+	if (argc)
+		printf("%s\n", argv[0]);
+	return (0);
+}

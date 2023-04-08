101f07b598a552eea92a028b2cf64eeeadbfc47f
Akachi <akachi.anabanti@gmail.com>
2023-04-08 15:55:37 +0000

    prints all the arguments received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the arguments in passed to it
+ * @argc: Argument count
+ * @argv: argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	while(argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}

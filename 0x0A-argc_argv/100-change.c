3b5e504a0f4655040c1def8b9d37b7fa6fabc99e
Akachi <akachi.anabanti@gmail.com>
2023-04-11 00:10:18 +0000

    change problem

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - calculates minimum number
+ * of coins required to make change
+ * usiing the greedy method for a given coin
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 on successful execution
+ */
+int main(int argc, char *argv[])
+{
+	int i, amount, coins_used = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 0; argv[1][i]; i++)
+	{
+		if (!isdigit(argv[1][i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	amount = atoi(argv[1]);
+
+	if (amount < 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 0; i < 5 && amount > 0; i++)
+	{
+		while (coins[i] <= amount)
+		{
+			amount -= coins[i];
+			coins_used++;
+		}
+	}
+	printf("%d\n", coins_used);
+	return (0);
+}

fcaf27783efaba08f8168c9cbcfdf9cf60051606
Akachi <akachi.anabanti@gmail.com>
2023-04-11 16:27:59 +0000

    fixed NULL input check

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,9 +13,6 @@ char *_strdup(char *str)
 
 	int len = 0;
 
-	if (*str == '\0')
-		return (NULL);
-
 	if (str == NULL)
 		return (NULL);
 
@@ -23,15 +20,13 @@ char *_strdup(char *str)
 	{
 		len++;
 	}
-	if (len <= 0)
-		return (NULL);
-	ptr = malloc(sizeof(char) * len + 1);
+	ptr = malloc(sizeof(char) * (len + 1));
 
 	if (ptr == NULL)
 		return (NULL);
 
-	for (; len >= 0; len--)
-		ptr[len] = str[len];
+	for (; len > 0; len--)
+		ptr[len - 1] = str[len - 1];
 
 	return (ptr);
 }

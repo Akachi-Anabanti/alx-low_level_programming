c92b47b501674597e48a7479fa52015c8efcf859
Akachi <akachi.anabanti@gmail.com>
2023-04-11 16:05:57 +0000

    fixed right amt of memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 
-	if(s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	while (s1[len_s1] != '\0')
@@ -28,7 +28,7 @@ char *str_concat(char *s1, char *s2)
 	while (s2[len_s2] != '\0')
 		len_s2++;
 
-	str = malloc(sizeof(char) * (len_s1 + len_s2));
+	str = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 
 	if (str == NULL)
 		return (NULL);

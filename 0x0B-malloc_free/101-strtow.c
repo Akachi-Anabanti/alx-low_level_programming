134c0a2b5856a505c6062b6d8d060f1af53f1b5f
Akachi <akachi.anabanti@gmail.com>
2023-04-13 13:53:37 +0000

    test for string with 0 charater

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -78,9 +78,8 @@ char **strtow(char *str)
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
+
 	num_words = count_words(str);
-	if (num_words < 1)
-		return (NULL);
 	words = malloc(sizeof(char *) * (num_words + 1));
 
 	if (words == NULL)

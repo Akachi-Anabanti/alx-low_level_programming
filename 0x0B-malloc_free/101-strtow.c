049dc0ebd700ebed5e4707cdd69e7c8eb8c6cb4f
Akachi <akachi.anabanti@gmail.com>
2023-04-13 13:48:06 +0000

    test for string with 0 charater

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -76,9 +76,13 @@ char **strtow(char *str)
 	int num_words, word_len, j, i = 0;
 	char **words, *word, *word_start, *word_end;
 
-	if (str == NULL || *str == '\0' || (*str == ' ' && !str[1]))
+	if (str == NULL || *str == '\0')
 		return (NULL);
 	num_words = count_words(str);
+
+	if (num_words < 1)
+		return (NULL);
+
 	words = malloc(sizeof(char *) * (num_words + 1));
 
 	if (words == NULL)
@@ -87,28 +91,25 @@ char **strtow(char *str)
 	{
 		if (is_delim(*str))
 			str++;
-		else
-		{
-			word_start = next_word(str), word_end = end_word(word_start);
-			word_len = word_end - word_start;
+		word_start = next_word(str), word_end = end_word(word_start);
+		word_len = word_end - word_start;
 
-			if (word_len > 0)
-				word = malloc(sizeof(char) * (word_len + 1));
-			if (word == NULL)
-			{
-				for (j = 0; j < i; j++)
-					free(words[j]);
-				free(words);
-				return (NULL);
-			}
-			for (j = 0; j < word_len; j++)
-			{
-				word[j] = *(word_start + j);
-			}
-			word[word_len] = '\0';
-			words[i++] = word;
-			str = word_end;
+		if (word_len > 0)
+			word = malloc(sizeof(char) * (word_len + 1));
+		if (word == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(words[j]);
+			free(words);
+			return (NULL);
 		}
+		for (j = 0; j < word_len; j++)
+		{
+			word[j] = *(word_start + j);
+		}
+		word[word_len] = '\0';
+		words[i++] = word;
+		str = word_end;
 	}
 	words[i] = NULL;
 	return (words);

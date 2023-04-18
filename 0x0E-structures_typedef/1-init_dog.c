3aa2877485385a1274b3451da747e79aae2a2c01
Akachi <akachi.anabanti@gmail.com>
2023-04-18 00:15:41 +0000

    structs in c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - initializes dog variable
+ * @d: pointer to dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owners name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _TASK_ZERO_
+#define _TASK_ZERO_
+/**
+ * struct dog - Dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif

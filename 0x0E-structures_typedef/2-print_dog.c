9155f7c39c2e94f35581736ed6e6d21cacf0388e
Akachi <akachi.anabanti@gmail.com>
2023-04-19 15:49:34 +0000

    print member of a struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_dog - prints the infos about the dog
+ * @d: the pointer to the dog struct
+ */
+void print_dog(struct dog *d)
+{
+	struct dog *ptr;
+
+	ptr = d;
+
+	if (ptr ==  NULL)
+		printf(" ");
+	if (ptr->name == NULL)
+	{
+		printf("Name: (nil)");
+		printf("Age: %.6f\n", ptr->age);
+		printf("Owner: %s\n", ptr->owner);
+	}
+	else if (!ptr->age)
+	{
+		printf("Name: %s\n", ptr->name);
+		printf("Age: (nil)");
+		printf("Owner: %s\n", ptr->owner);
+	}
+
+	else if (ptr->owner == NULL)
+	{
+		printf("Name: %s\n", ptr->name);
+		printf("Age: %.6f\n", ptr->age);
+		printf("Owner: (nil)");
+	}
+	else
+	{
+		printf("Name: %s\n", ptr->name);
+		printf("Age: %.6f\n", ptr->age);
+		printf("Owner: %s\n", ptr->owner);
+	}
+}

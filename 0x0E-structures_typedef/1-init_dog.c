b89be19baebc34dc1e3bd801f33f92d830b7aa33
Akachi <akachi.anabanti@gmail.com>
2023-04-19 12:41:52 +0000

    changed -> to deference

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,11 +8,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog dog;
-
-	d = &dog;
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }

bae1b1225d2d27c059ec170ca1c1d60a1f7e6054
Akachi <akachi.anabanti@gmail.com>
2023-04-19 12:56:42 +0000

    added conditional

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,8 +10,11 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	struct dog *ptr;
 
-	ptr = d;
-	(*ptr).name = name;
-	(*ptr).age = age;
-	(*ptr).owner = owner;
+	if (d)
+	{
+		ptr = d;
+		(*ptr).name = name;
+		(*ptr).age = age;
+		(*ptr).owner = owner;
+	}
 }

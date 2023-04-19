26d7d7c7ca7717f0cd99f46e1c596e4fde49992d
Akachi <akachi.anabanti@gmail.com>
2023-04-19 16:32:16 +0000

    code refactor

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stddef.h>
 #include <stdio.h>
 /**
  * print_dog - prints the infos about the dog
@@ -7,30 +6,12 @@
  */
 void print_dog(struct dog *d)
 {
-	struct dog *ptr;
 
 	if (!d)
 		return;
-	ptr = d;
-
-	if (!ptr)
-		return;
-	if (!(ptr->name))
-	{
-		printf("Name: (nil)\n");
-		printf("Age: %.6f\n", ptr->age);
-		printf("Owner: %s\n", ptr->owner);
-	}
-	else if (!(ptr->owner))
-	{
-		printf("Name: %s\n", ptr->name);
-		printf("Age: %.6f\n", ptr->age);
-		printf("Owner: (nil)\n");
-	}
-	else
-	{
-		printf("Name: %s\n", ptr->name);
-		printf("Age: %.6f\n", ptr->age);
-		printf("Owner: %s\n", ptr->owner);
-	}
+	if (!d->name)
+		d->name = "(nil)";
+	if (!d->owner)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }

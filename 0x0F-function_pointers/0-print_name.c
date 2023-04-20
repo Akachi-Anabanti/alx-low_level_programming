57864afba32007d1400ab5b55067fc4ab4d8dec1
Akachi <akachi.anabanti@gmail.com>
2023-04-20 07:10:45 +0000

    function pointer to print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - A function that prints name
+ * @name: name of person
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

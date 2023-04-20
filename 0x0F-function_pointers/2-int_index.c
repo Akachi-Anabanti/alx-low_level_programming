6d080c44bcb502cde583d9f34d5ea26ce104b4f8
Akachi <akachi.anabanti@gmail.com>
2023-04-20 23:29:50 +0000

    find index of a given int in array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - returns index of an integer
+ * @array: array of elements
+ * @size: size of the array
+ * @cmp: pointer to function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i <= size; i++)
+	{
+		if (i == size && !cmp(array[i]))
+			return (-1);
+		else if (cmp(array[i]))
+			break;
+	}
+	return (i);
+}

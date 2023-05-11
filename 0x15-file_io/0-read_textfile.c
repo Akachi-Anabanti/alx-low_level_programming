020fc98c2c03b7b0b8be50e63bec07934fda4ba9
Akachi <akachi.anabanti@gmail.com>
2023-05-11 11:59:42 +0000

    read files from a text and return the number of chars printed of stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file
+ * @fiilename: name of the file
+ * @letters: Number of letters it should print
+ * Returns: The actual number od letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	size_t i;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	buff = malloc(letters);
+
+	if (buff == NULL)
+		return (0);
+
+	i = read(fd, buff, letters);
+	if (write(STDIN_FILENO, buff, i) == -1)
+		return (0);
+	letters =  i;
+	close(fd);
+	free(buff);
+	return (letters);
+}

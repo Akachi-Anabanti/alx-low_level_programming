ec8425eda9f5b768c5f9599848f35b031cf73c6e
Akachi <akachi.anabanti@gmail.com>
2023-05-11 14:18:57 +0000

    fixes: condition logic and typo

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,11 +18,13 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
+	if (text_content == NULL && fd == 1)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
+	else if (text_content == NULL)
+		return (-1);
 	for (len = 0; text_content[len] != '\0'; len++)
 		;
 	if (write(fd, text_content, len) == -1)

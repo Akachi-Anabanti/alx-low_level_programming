f55a2f4690cc32374dac59ae0d51211c77c8767d
Akachi <akachi.anabanti@gmail.com>
2023-05-11 14:34:36 +0000

    minor fixes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,12 +18,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL && fd == -1)
 		return (-1);
-	else if (text_content == NULL && fd == 1)
+	if (text_content == NULL && fd == 1)
 	{
 		close(fd);
 		return (1);
 	}
-	else if (fd == -1)
+	if (fd == -1)
 		return (-1);
 	for (len = 0; text_content[len] != '\0'; len++)
 		;

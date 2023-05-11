8260268d4fc8bdb2e044a38f3cc0f42fb41c504f
Akachi <akachi.anabanti@gmail.com>
2023-05-11 13:32:13 +0000

    recalculate strlen

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,9 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	size_t len;
 	mode_t mode = S_IRUSR | S_IWUSR;
+
 
 	if (filename == NULL)
 		return (-1);
@@ -22,8 +24,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		return (-1);
-	if (write(fd, text_content, strlen(text_content)) == -1)
+	{
+		close(fd);
+		return (1);
+	}
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+	if (write(fd, text_content, len) == -1)
 	{
 		close(fd);
 		return (-1);

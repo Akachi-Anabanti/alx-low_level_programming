c62cada9dea40e404d5a70df53bcef2ca1b1d8d0
Akachi <akachi.anabanti@gmail.com>
2023-05-11 14:28:25 +0000

    fixes for logic and return statement

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,14 +16,14 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (fd == -1)
+	if (text_content != NULL && fd == -1)
 		return (-1);
-	if (text_content == NULL && fd == 1)
+	else if (text_content == NULL && fd == 1)
 	{
 		close(fd);
 		return (1);
 	}
-	else if (text_content == NULL)
+	else if (fd == -1)
 		return (-1);
 	for (len = 0; text_content[len] != '\0'; len++)
 		;
@@ -32,6 +32,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
-	close(fd);
+	if (close(fd) == -1)
+		return (-1);
 	return (1);
 }

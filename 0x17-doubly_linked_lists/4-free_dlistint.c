d8fc785fa82eb9b9a562edf740eee23b878266ed
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-10 21:52:36 +0000

    free linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a doubly linked list
+ * @head: Head of the node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp, *next_node;
+
+	if (head == NULL)
+		return;
+	temp = head;
+	while (temp != NULL)
+	{
+		next_node =  temp->next;
+		free(temp);
+		temp = next_node;
+	}
+	free(next_node);
+}

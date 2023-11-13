7a1ecf88b0e91940cafe4ab7666bf83acbf35749
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-13 23:28:56 +0000

    reverted change to download path and corrected bin path

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-wget https://github.com/alx-tools/0x18.c
-export LD_PRELOAD=./0x18.c ./gm 9 8 10 24 75 9
+#!/bin/bash
+wget -P https://github.com/alx-tools/0x18.c
+export LD_PRELOAD=/tmp/0x18.c ./gm 9 8 10 24 75 9

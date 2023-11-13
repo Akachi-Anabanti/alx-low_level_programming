948f8abe4584eae48378bc41cb02b6e3b9dd7397
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-13 23:57:05 +0000

    update to download so file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/alx-tools/0x18.c
-export LD_PRELOAD=/tmp/0x18.c/gm 9 8 10 24 75 9
+wget -P /tmp https://github.com/Akachi-Anabanti/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so

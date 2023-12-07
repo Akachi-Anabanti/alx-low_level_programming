22c40f0b22c8c5e45cca8750aef47b2b58417afd
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-07 15:32:15 +0000

    updated

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,7 +14,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 
-	if (ht->array[index] == NULL)
+	if (!ht->array[index])
 		return (NULL);
 	return (ht->array[index]->value);
 }

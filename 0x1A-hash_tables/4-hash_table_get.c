a6e970bd340e48b1021c0e2e423c77d8abc0974e
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-07 15:29:55 +0000

    get value from key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht: The hash table
+ * @key: The key
+ * Return: The value associated with the key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[index] == NULL)
+		return (NULL);
+	return (ht->array[index]->value);
+}
